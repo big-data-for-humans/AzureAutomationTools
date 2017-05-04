9ae682ca69378986358882e2d2576097f9591865
Liam Fisher <LiamF@bigdataforhumans.com>
2017-05-04 13:03:23 +0100

    Updated build-package-management to take multiple packages into account

--- build-package-management.ps1
+++ build-package-management.ps1
@@ -1,34 +1,80 @@
 
 $ErrorActionPreference = 'Stop'
 
-$OutputFolder = 'out\PackageManagement'
-$SourceFolder = 'src\PackageManagement'
-$DocsFolder = 'docs\PackageManagement'  
+$Namespace = 'AzureAutomation'
+$Packages = @(
+    @{
+        SourceFolder = 'src\PackageManagement'
+        DocsFolder = "docs\PackageManagement"
+    },
+    @{
+        SourceFolder = 'src\Logging'
+        DocsFolder = 'docs\Logging'
+    }
+) 
 
 Import-Module -Name PSScriptAnalyzer
 
-$Results = Invoke-ScriptAnalyzer -Path src\PackageManagement\AzureAutomationTools.PackageManagement.psm1 -Settings 'CodeFormatting'
 
-if($Results.Count -ne 0){
-    $Results 
-    throw 'Failed CodeFormatting rules'
+foreach ($Package in $Packages) {
+    $SourceFolder = $Package.SourceFolder
+    $SourceFolderLiteral = Join-Path -Path $PSScriptRoot -ChildPath $SourceFolder
+    $DocsFolder = $Package.DocsFolder
+    $DocsFolderLiteral = Join-Path -Path $PSScriptRoot -ChildPath $DocsFolder
+
+    if (-not (Test-Path -Path $SourceFolderLiteral)) {
+        throw "Cannot find package src path: '$SourceFolderLiteral'. Please edit packages in build script!"
+    }
+
+    $Psd1Path = Get-ChildItem -Path $SourceFolderLiteral -Filter '*.psd1'
+    if ($null -eq $Psd1Path) {
+        throw "Cannot find psd1 file in package location: '$SourceFolderLiteral'"
+    }
+    if ($Psd1Path.count -gt 1) {
+        throw "Multiple .psd1 files found in path '$SourceFolderLiteral'."
+    }
+
+    if ($Package.Keys -notcontains 'OutputFolder') {
+        $OutputFolder = Join-Path -Path 'out' -ChildPath $Psd1Path.BaseName
+    }
+    else {
+        $OutputFolder = $Package.OutputFolder
+    }
+
+    $Psd1Obj = Import-LocalizedData -BaseDirectory $Psd1Path.Directory -FileName $Psd1Path.Name
+    $Psm1Path = Join-Path -Path $SourceFolderLiteral -ChildPath $Psd1Obj.RootModule
+    $Results = Invoke-ScriptAnalyzer -Path $Psm1Path -Settings 'CodeFormatting'
+
+    if($Results.Count -ne 0){
+        $Results 
+        Write-Warning "The file '$Psm1Path' Failed CodeFormatting rules. This PR will not be accepted in its current state!"
+    }
+
+    if(Test-Path -Path $OutputFolder -PathType Any){
+        Remove-Item -Path $OutputFolder -Recurse
+    }
+    New-Item -Path $OutputFolder -ItemType Container
+
+    Copy-Item -Path $SourceFolder\* -Destination $OutputFolder -Recurse
+
+    # Test the import on a new PowerShell session
+    $Ps = [scriptblock]::Create("Import-Module -Name $($Psd1Path.FullName)").GetPowerShell()
+    $Ps.Invoke()
+
+    if ($Ps.HadErrors) {
+        $Exception = [Exception]::new("Failed to import module '$($Psd1Path.FullName)'. Reason: $($Ps.Streams.Error[0].Exception.Message)", $Ps.Streams.Error[0].Exception)
+        throw $Exception
+    }
+
+    if ($null -eq $DocsFolder) {
+        Write-Warning -Message "Package '$($Package.SourceFolder)' does not contain a DocsFolder parameter. This PR will not be accepted in its current state!"
+    }
+    elseif (-not (Test-Path -Path $DocsFolderLiteral)) {
+        Write-Warning -Message "Package '$($Package.SourceFolder)' contains a DocsFolder parameter but the path '$DocsFolderLiteral' does not exist! This PR will not be accepted in its current state!"
+    }
+    else {
+        New-ExternalHelp -Path $DocsFolderLiteral -OutputPath "$OutputFolder\en-US"
+    }
+    
+    Write-Verbose -Message (Resolve-Path $OutputFolder) -Verbose
 }
-
-# excluding PSUseDeclaredVarsMoreThanAssignments as seems to false positive on += to arrays
-$Results = Invoke-ScriptAnalyzer -Path src\PackageManagement\AzureAutomationTools.PackageManagement.psm1 -Settings 'PSGallery' -ExcludeRule 'PSUseDeclaredVarsMoreThanAssignments'
-
-if($Results.Count -ne 0){
-    $Results 
-    throw 'Failed PSGallery rules'
-}
-
-if(Test-Path -Path $OutputFolder -PathType Any){
-    Remove-Item -Path $OutputFolder -Recurse
-}
-
-New-Item -Path $OutputFolder -ItemType Container
-Copy-Item -Path $SourceFolder\* -Destination $OutputFolder -Recurse
-Import-Module (Resolve-Path $OutputFolder\AzureAutomationTools.PackageManagement.psd1)
-New-ExternalHelp -Path $DocsFolder -OutputPath "$OutputFolder\en-US"
-
-Write-Verbose -Message (Resolve-Path $OutputFolder) -Verbose
